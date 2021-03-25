13ba8b71bce82a8c200502515d72da2955f7a30b
martinjacquelard <martinjacquelard@free.fr>
2021-03-25 13:25:28 +0100

    moore-dijkstra ok

--- C2/OPTI/test_simplex.py
+++ C2/OPTI/test_simplex.py
@@ -14,7 +14,6 @@ def test1():
 
     s = PLSimplex(xi,ei,Z,C,S)
     s.launch()
-
 def test2():
     # Initialisations
     if 1 :
@@ -29,7 +28,6 @@ def test2():
 
     s = PLSimplex(xi,ei,Z,C,S)
     s.launch()
-
 def test3():
     # Initialisations
     if 1 :
@@ -43,22 +41,43 @@ def test3():
 
     s = PLSimplex(xi,ei,Z,C,S)
     s.launch()
-
 def test4():
     # Initialisations
     if 1 :
         xi = ["x1","x2"]
-        ei = ["e1","e2","e3"]
-        Z  = [0,0,0,3,5,0]
-        C  = [[1.0,0.0,0.0,1.0,0.0],
-              [0.0,1.0,0.0,0.0,1.0],
-              [0.0,0.0,1.0,3.0,2.0]]
-        S  = [4,6,18]
+        ei = ["e1","e2"]
+        Z  = [0,0,2,1,0]
+        C  = [[1,0,2,4],
+              [0,1,3,2]]
+        S  = [12,12]
 
     s = PLSimplex(xi,ei,Z,C,S)
-    fig = s.launch_graph(8,8)
+    fig = s.launch_graph(5,5)
     fig.show()
+    s.launch()
+    return
+def entrainement_partiel():
+    # initialisation
+    if 1 :
+        xi = ["x1", "x2"]
+        ei = ["e1", "e2", "e3"]
+        Z = [0, 0, 0, 10, 6, 0]
+        C = [[1, 0, 0, 1, 1],
+             [0, 1, 0, 2, 1],
+             [0, 0, 1, 0, 1]]
+        S = [15, 20, 12]
+        s = PLSimplex(xi, ei, Z, C, S)
+
+    #Q4
+    s.launch_graph(20,20)
+
+    #Q4b
+    s.launch()
+
+    return
 
 if __name__ == '__main__':
+    #
     #test2()
     test4()
+    #entrainement_partiel()
